b4f17243286623d822c75666a10576d3bce6b736
DEVON DAVIAU <devon.daviau@yahoo.ca>
2017-03-27 20:15:34 -0400

    configure routing for home component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,10 +5,13 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // import child components
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   // add component pathing
-  { path: 'home', component: componentName }
+  { path: 'home', component: HomeComponent },
+  // default redirect
+  { path: '', component: HomeComponent, pathMatch: 'full' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by devon on 2017-03-27.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home/home.component.html'
+})
+
+export class HomeComponent {
+  title: 'Home Page';
+}
