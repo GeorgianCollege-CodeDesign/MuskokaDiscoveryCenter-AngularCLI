6c9e6e91a2253d1bc77434d5dfb247ad72bbf465
Esat IBIS <esat.taha.ibis@gmail.com>
2017-03-27 22:29:10 -0400

    Camper and Account service added. Http get call added to the camper service call.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CamperService } from "./camper.service";
+import {Camper} from "./camper";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CamperService]
 })
 
 export class AppComponent {
   title = 'app works!';
+  campers: Camper[];
+  errorMessage: any;
+  constructor(private camperService: CamperService) {
+    //this.getCampers();
+  }
+
+  getCampers() {
+    this.camperService.getCampers()
+      .subscribe(
+        campers => this.campers = campers,
+        error =>  this.errorMessage = <any>error);
+    console.log(this.campers);
+  }
 }
---
+++ src/app/camper.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by Esat IBIS on 2017-03-27.
+ * Project: muskoka-discovery-center.
+ * @author: Esat IBIS <esat.taha.ibis@gmail.com>
+ */
+
+export class Camper {
+  id: string;
+  camperFirstName: string;
+  camperLastName: string;
+  parentFirstName: string;
+  parentLastName: string;
+  parentPhoneNumber : string;
+  paymentType : string;
+  camperAge: number;
+  camperNotes : string;
+  camperPickupList : [
+    {
+      firstName: string,
+      lastName: string
+    }
+  ]
+}
