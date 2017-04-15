1f1b8f564220f44b962211d94e9db3642ee9f42f
Esat IBIS <esat.taha.ibis@gmail.com>
2017-04-14 21:37:22 -0400

    Edit camper added. and it's working.

--- src/app/camper-edit/camper-edit.component.ts
+++ src/app/camper-edit/camper-edit.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {CamperService} from "../camper.service";
+import {Camper} from "../camper";
 
 @Component({
   selector: 'app-camper-edit',
   templateUrl: './camper-edit.component.html',
   styleUrls: ['./camper-edit.component.css']
 })
+
 export class CamperEditComponent implements OnInit {
 
-  constructor() { }
+  _id: string;
+  camperFirstName: string = '';
+  camperLastName: string= '';
+  parentFirstName: string = '';
+  parentLastName: string = '';
+  parentPhoneNumber : string = '';
+  paymentDays: number = 3;
+  camperAge: number = 3;
+  camperNotes : string = '';
+  camperPickupList : Array<any> = [];
+  startDate: number = 3;
+  endDate: number = 3;
+  absenceDays: Array<any> = [];
+
+  pickupHistory: Array<any>;  constructor(
+    private activatedRoute: ActivatedRoute,
+    private camperService: CamperService) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      let camperID = params['id'];
+      console.log(camperID);
+      this._id = camperID;
+      this.camperService.getCamper(camperID)
+        .subscribe(res => {
+          this._id = res._id;
+          this.camperFirstName = res.camperFirstName;
+          this.camperLastName = res.camperLastName;
+          this.parentFirstName = res.parentFirstName ;
+          this.parentLastName = res.parentLastName;
+          this.parentPhoneNumber = res.parentPhoneNumber;
+          this.paymentDays = res.paymentDays;
+          this.camperAge = res.camperAge;
+          this.camperNotes = res.camperNotes;
+          this.camperPickupList = res.camperPickupList;
+          this.startDate = res.startDate;
+          this.endDate = res.endDate;
+          this.absenceDays = res.absenceDays;
+        })
+    });
   }
 
 }
--- src/app/camper.ts
+++ src/app/camper.ts
@@ -5,20 +5,19 @@
  */
 
 export class Camper {
-  _id: string;
-  camperFirstName: string;
-  camperLastName: string;
-  parentFirstName: string;
-  parentLastName: string;
-  parentPhoneNumber : string;
-  paymentType : string;
-  camperAge: number;
-  camperNotes : string;
-  camperPickupList : [
-    {
-      firstName: string,
-      lastName: string,
-      _id: string
-    }
-  ]
+    _id: string;
+    camperFirstName: string;
+    camperLastName: string;
+    parentFirstName: string;
+    parentLastName: string;
+    parentPhoneNumber : string;
+    paymentDays: number;
+    camperAge: number;
+    camperNotes : string;
+    camperPickupList : Array<any>;
+    startDate: number;
+    endDate: number;
+    absenceDays: Array<any>;
+    isActive: boolean;
+    pickupHistory: Array<any>;
 }
