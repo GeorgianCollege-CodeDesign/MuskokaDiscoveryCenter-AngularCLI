18723bced78fb71748707c8ec18b46c4901f4547
Esat IBIS <esat.taha.ibis@gmail.com>
2017-04-14 23:47:57 -0400

    Camper service is finished.

--- src/app/camper.service.ts
+++ src/app/camper.service.ts
@@ -8,29 +8,42 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class CamperService {
 
-  private camper = '/api/campers';  // URL to web API
-
+  private _camper = '/api/campers';  // URL to web API
+  private _activeCampers = '/api/active-campers';
+  private _searchCamper = '/api/camper';
+  private _camperSignin = '/api/camper-sign-in';
+  private _camperSignout = '/api/camper-sign-out';
+  private _dailyCampers = '/api/daily-campers';
 
   constructor(private http: Http) { }
 
   getCampers(): Observable<Camper[]> {
-    return this.http.get(this.camper)
-        .map(this.extractData)
-        .catch(this.handleError);
+    return this.http.get(this._camper)
+        .map(CamperService.extractData)
+        .catch(CamperService.handleError);
   }
 
   getCamper(id: number): Observable<Camper> {
-    return this.http.get(`${this.camper}/${id}`)
-      .map(this.extractData)
-      .catch(this.handleError);
+    return this.http.get(`${this._camper}/${id}`)
+      .map(CamperService.extractData)
+      .catch(CamperService.handleError);
   }
 
-  putCamper(body: Object, _id: string): any {
-    let bodyString = JSON.stringify(body); // Stringify payload
+  createCamper(body: Object){
     let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
     let options       = new RequestOptions({ headers: headers }); // Create a request option
 
-    return this.http.put(`${this.camper}/${_id}`, body, options) // ...using put request
+    return this.http.post(this._camper, body, options) // ...using put request
+      .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if
+  }
+
+  putCamper(body: Object, _id: string): any {
+    //let bodyString = JSON.stringify(body); // Stringify payload
+    let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+    let options       = new RequestOptions({ headers: headers }); // Create a request option
+
+    return this.http.put(`${this._camper}/${_id}`, body, options) // ...using put request
       .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
       .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if
   }
@@ -41,12 +54,46 @@ export class CamperService {
     });
   }
 
-  private extractData(res: Response) {
+  getActiveCampers(){
+    return this.http.get(this._activeCampers)
+      .map(CamperService.extractData)
+      .catch(CamperService.handleError);
+  }
+
+  searchCamper(firstName: string, lastName: string) {
+    return this.http.get(`${this._searchCamper}/${firstName}/${lastName}`)
+      .map(CamperService.extractData)
+      .catch(CamperService.handleError);
+  }
+
+  getDailyCampers() {
+    return this.http.get(this._dailyCampers)
+      .map(CamperService.extractData)
+      .catch(CamperService.handleError);
+  }
+
+  camperSignIn(camperParent: string) {
+    let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+    let options       = new RequestOptions({ headers: headers }); // Create a request option
+    return this.http.post(this._camperSignin, {camperParent: camperParent},  options)
+      .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if
+  }
+
+  camperSignOut(camperParent: string) {
+    let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+    let options       = new RequestOptions({ headers: headers }); // Create a request option
+    return this.http.post(this._camperSignout, {camperParent: camperParent},  options)
+      .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if
+  }
+
+  private static extractData(res: Response) {
     let body = res.json();
     return body || { };
   }
 
-  private handleError (error: Response | any) {
+  private static handleError (error: Response | any) {
     let errMsg: string;
     if (error instanceof Response) {
       const body = error.json() || '';
