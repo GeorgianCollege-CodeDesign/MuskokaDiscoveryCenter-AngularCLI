2ada56b920981cbf8a13827732ab1fc3840c83b9
AkashdeepSIN <you@example.com>
2017-04-16 11:28:36 -0400

    add signout form

---
+++ src/app/signout-form/signout-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signout-form',
+  templateUrl: './signout-form.component.html',
+  styleUrls: ['./signout-form.component.css']
+})
+export class SignoutFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
