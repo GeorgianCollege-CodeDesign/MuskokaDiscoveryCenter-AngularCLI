b2820ef0817e85ed613df9dba7c9c276b55e2f8b
Esat IBIS <esat.taha.ibis@gmail.com>
2017-04-18 09:47:58 -0400

    Password change email content

--- server/routes/api.js
+++ server/routes/api.js
@@ -667,7 +667,7 @@ router.post('/forgot', function(req, res, next) {
       let mailOptions = {
         to: user.email,
         from: 'passwordreset@demo.com',
-        subject: 'Node.js Password Reset',
+        subject: 'Password Reset - Muskoka Discovery Center Day Camp',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
         'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
         'http://' + req.headers.host + '/reset/' + token + '\n\n' +
